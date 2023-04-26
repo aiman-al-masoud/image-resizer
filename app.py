4fd5db9104a7e834e45bf29860318991a1d8cf14
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-04-27 00:35:10 +0200

    MMMMMMMMMMMMMMMMMMMMMMMMMMm

--- app.py
+++ app.py
@@ -6,11 +6,9 @@ app = Flask(__name__, static_url_path='/static')
 # app = Flask(__name__, static_url_path='/')
 # print(app.static_folder)
 
-
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
-
 
 @app.route('/<path:path>')
 def static_file(path):
@@ -20,3 +18,22 @@ def static_file(path):
         return app.send_static_file('index.html')
 
     return app.send_static_file(path)
+
+
+@app.route("/share")
+def share():
+    print('sharing requested!')
+
+
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
--- static/index.js
+++ static/index.js
@@ -1,15 +1,21 @@
+document.write('serviceWorker' in navigator)
+
 if ('serviceWorker' in navigator) {
 
-    navigator.serviceWorker.register('./service-worker.js')
+    navigator.serviceWorker.register('/static/service-worker.js')
         .then(function (reg) {
-            console.log("Service worker registered.")
+            // console.log("Service worker registered.")
+            // confirm('SERVICE WORKER REGISTERE!')
+            document.write('YAY!')
         }).catch(function (err) {
-            console.log("Service worker not registered. This happened:", err)
+            document.write('NOOOOOOOOOO!', err)
         })
 }
 
+// document.write('SCEMO!')
+// alert('CIAO!')
 
-self.addEventListener('fetch', function (event) { })
-
-document.write('SCEMO!')
-alert('CIAO!')
+// var parsedUrl = new URL(window.location.toString());
+// console.log('Title shared: ' + parsedUrl.searchParams.get('name'));
+// console.log('Text shared: ' + parsedUrl.searchParams.get('description'));
+// console.log('URL shared: ' + parsedUrl.searchParams.get('link'));
