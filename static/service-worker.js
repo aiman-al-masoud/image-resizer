f790e5aa20aba7b2c8a240ce701396da27c4a993
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-04-27 01:32:11 +0200

    ...

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,13 +1,40 @@
 self.addEventListener('install', (event) => {
     console.log('👷', 'install', event);
     self.skipWaiting();
-  });
-  
-  self.addEventListener('activate', (event) => {
+});
+
+self.addEventListener('activate', (event) => {
     console.log('👷', 'activate', event);
     return self.clients.claim();
-  });
-  
-  self.addEventListener('fetch', function(event) {
+});
+
+self.addEventListener('fetch', function (event) {
     console.log('👷', 'fetch', event);
-  });
+});
+
+
+
+self.addEventListener('fetch', event => {
+
+    const url = new URL(event.request.url);
+
+    if (event.request.method === 'POST' && url.pathname === '/post-shared-content') {
+        event.respondWith((async () => {
+            const data = await event.request.formData();
+
+            const filename = data.get('title');
+            const file = data.get('photos');
+
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                // const textContent = e.target.result;
+                // Do something with the textContent here.
+
+                document.write(filename)
+
+            };
+            // reader.readAsText(file);
+            // return Response.redirect('/snippet-stored-success', 303);
+        })());
+    }
+});
